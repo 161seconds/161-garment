b463fce0cbfe4a60afb170a49e0171c6623d6e7d
161seconds <quocbao.nguyen16102006@gmail.com>
2026-08-15 13:53:19 +0700

    Fix Tomcat invalid shutdown command warning on Render by disabling shutdown port

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 PORT="${PORT:-8080}"
 echo "Configuring Tomcat to listen on port ${PORT}..."
 
+# Disable Tomcat shutdown socket (port 8005 -> -1) so Render health checks do not trigger "Invalid shutdown command"
+sed -i 's/<Server port="8005"/<Server port="-1"/g' /usr/local/tomcat/conf/server.xml
+
 # Replace default port 8080 with dynamic $PORT in server.xml
 sed -i "s/8080/${PORT}/g" /usr/local/tomcat/conf/server.xml
 
